f59d63cb0563ea96a7854832a2ebe96005d93fc1
RickHayden <richard.hayden459@live.com>
2022-10-05 15:28:11 -0400

    progress made

---
+++ main_app/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+import time
+#...
+
+class Artist(models.Model):
+
+    name = models.CharField(max_length=150)
+    img = models.CharField(max_length=250)
+    bio = models.TextField(max_length=500)
+    verified_artist = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+# below Artist Model
+
+class Song(models.Model):
+
+    title = models.CharField(max_length=150)
+    length = models.IntegerField(default=0)
+    artist = models.ForeignKey(
+        Artist, on_delete=models.CASCADE, related_name="songs")
+
+    def __str__(self):
+        return self.title
+
+    # Here we define the method to look at the length property and convert it
+    def get_length(self):
+        return time.strftime("%-M:%S", time.gmtime(self.length))
